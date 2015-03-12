acdff5614652889fecdbcbdee1e994007c78deb3
Treemo <treemo@hotmail.fr>
2015-03-12 17:58:12 +0100

    use decorator ascyn

--- Module.py
+++ Module.py
@@ -1,4 +1,5 @@
 from core import Log
+from core.decorator import async
 
 from circuits import Component, Event, handler
 from email.mime.text import MIMEText
@@ -52,16 +53,18 @@ class Module(Component):
 
         Log.info('%d account load' % nb)
 
+    @async
     def login(self, account=''):
         config = self.__get_config(account)
 
         try:
-            smtp_constructor_keys = ['host', 'port', 'local_hostname', \
-                'timeout', 'source_address']
+            smtp_constructor_keys = ['host', 'port', 'local_hostname',
+                                     'timeout', 'source_address']
 
-            smtp_kwarg = {key: config[key] for key in smtp_constructor_keys if key in config}
+            smtp_kwarg = {key: config[key] for key in smtp_constructor_keys if
+                          key in config}
             conn = smtplib.SMTP(**smtp_kwarg)
-            #conn.ehlo()
+            # conn.ehlo()
             #conn.starttls()
 
             if 'debug' in config:
@@ -86,10 +89,11 @@ class Module(Component):
 
     @handler('send_mail', channel='*')
     def send_mail(self, subject, to, msg, account=''):
-        conn = self.login(account)
+        conn = (yield self.login(account)).value
+        Log.debug('mail connection: %s' % str(conn))
 
         if not conn:
-            return
+            raise StopIteration
 
         config = self.__get_config(account)
 
@@ -102,15 +106,33 @@ class Module(Component):
 
         if not 'from' in config:
             config['from'] = config['login']
-            
+
         if 'name' in config:
             mail['From'] = '%(name)s <%(from)s>' % config
 
         else:
             mail['From'] = config['from']
 
+        yield self.__send_mail(conn, account, to, mail.as_string())
+        conn.close()
+
+    def started(self, component):
+        self.load_configuration()
+
+    def __get_config(self, account=''):
+        if not account:
+            account = self.default_account
+
+        if not account in self.__account_config:
+            Log.error('smtp account "%s" unknown' % account)
+            return
+
+        return self.__account_config[account]
+
+    @async
+    def __send_mail(self, conn, account, to, body):
         try:
-            conn.sendmail(account, to, mail.as_string())
+            conn.sendmail(account, to, body)
 
         except smtplib.SMTPRecipientsRefused as e:
             # All recipients were refused. Nobody got the mail. The recipients attribute of the exception object is a dictionary with information about the refused recipients (like the one returned when at least one recipient was accepted).
@@ -127,20 +149,3 @@ class Module(Component):
         except smtplib.SMTPDataError as e:
             # The server replied with an unexpected error code (other than a refusal of a recipient).
             Log.error('smtp data error: %s' % str(e))
-
-        finally:
-            conn.close()
-
-    def started(self, component):
-        self.load_configuration()
-
-    def __get_config(self, account=''):
-        if not account:
-            account = self.default_account
-
-        if not account in self.__account_config:
-            Log.error('smtp account "%s" unknown' % account)
-            return
-
-        return self.__account_config[account]
-
